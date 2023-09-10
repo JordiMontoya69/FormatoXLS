53bd6fe65de7d4beb766574f6ce9bec00867213f
JordiMontoya69 <114711603+JordiMontoya69@users.noreply.github.com>
2023-09-10 17:04:14 -0600

    Add files via upload

---
+++ FormatoXLSvb/Form1.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.button1 = New System.Windows.Forms.Button()
+        CType(Me.dataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dataGridView1
+        '
+        Me.dataGridView1.AllowUserToAddRows = False
+        Me.dataGridView1.AllowUserToDeleteRows = False
+        Me.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.dataGridView1.Name = "dataGridView1"
+        Me.dataGridView1.ReadOnly = True
+        Me.dataGridView1.Size = New System.Drawing.Size(530, 293)
+        Me.dataGridView1.TabIndex = 3
+        '
+        'button1
+        '
+        Me.button1.Location = New System.Drawing.Point(455, 315)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(87, 31)
+        Me.button1.TabIndex = 2
+        Me.button1.Text = "Crear Reporte"
+        Me.button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(570, 360)
+        Me.Controls.Add(Me.dataGridView1)
+        Me.Controls.Add(Me.button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Private WithEvents dataGridView1 As DataGridView
+    Private WithEvents button1 As Button
+End Class
---
+++ FormatoXLSvb/Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Data.SqlClient
+Imports Excel = Microsoft.Office.Interop.Excel
+Imports System.IO
+
+Public Class Form1
+    Private Sub button1_Click(sender As Object, e As EventArgs) Handles button1.Click
+        Dim guardar As SaveFileDialog = New SaveFileDialog()
+        Dim app As Excel.Application = New Excel.Application()
+        Dim work As Excel.Workbook = app.Workbooks.Open(Path.GetFullPath("club nautico.xlsx"), Nothing, True)
+        Dim sheet As Excel.Worksheet = work.Worksheets(1)
+        Dim j As Integer = 2
+        Dim n As Integer = dataGridView1.Rows.Count
+
+
+
+        For i As Integer = 0 To n - 1
+            sheet.Range("A" & j.ToString()).Value = j - 1
+            sheet.Range("B" & j.ToString()).Value = dataGridView1.Rows(i).Cells(0).Value
+            sheet.Range("C" & j.ToString()).Value2 = dataGridView1.Rows(i).Cells(1).Value
+            sheet.Range("D" & j.ToString()).Value2 = dataGridView1.Rows(i).Cells(2).Value
+            sheet.Range("E" & j.ToString()).Value2 = dataGridView1.Rows(i).Cells(3).Value
+            sheet.Range("F" & j.ToString()).Value2 = dataGridView1.Rows(i).Cells(4).Value
+            sheet.Range("G" & j.ToString()).Value2 = dataGridView1.Rows(i).Cells(5).Value
+            sheet.Range("H" & j.ToString()).Value2 = dataGridView1.Rows(i).Cells(6).Value
+
+
+
+            If i < n - 1 Then
+                sheet.Range("A" & (j + 1).ToString()).EntireRow.Insert(Excel.XlDirection.xlDown, Excel.XlInsertFormatOrigin.xlFormatFromLeftOrAbove)
+            End If
+
+
+
+            j += 1
+        Next
+
+
+
+        app.Visible = True
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim conexion As SqlConnection = New SqlConnection("Server='localhost\SQLEXPRESS'; Database='club_nautico';Trusted_Connection=True;")
+        conexion.Open()
+        Dim query As SqlCommand = New SqlCommand("select * from barco", conexion)
+        Dim datos As SqlDataReader = query.ExecuteReader()
+        Dim ds As DataSet = New DataSet()
+        ds.Load(datos, LoadOption.OverwriteChanges, "datos")
+        dataGridView1.DataSource = ds.Tables("datos")
+    End Sub
+End Class
